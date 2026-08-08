29d45f553b5d2b09b86db43f181f481d0a581d50
Daniel Steven Lopez Perez <dast11lp@gmail.com>
2026-08-08 04:17:54 -0500

    intento1 cors aws

--- src/main/java/com/alumni/config/WebConfig.java
+++ src/main/java/com/alumni/config/WebConfig.java
@@ -10,7 +10,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://127.0.0.1:5502", "http://localhost:5502")
+                .allowedOrigins(
+                        "http://127.0.0.1:5502",
+                        "http://localhost:5502",
+                        "http://127.0.0.1:5501",
+                        "http://localhost:5501",
+                        "https://ec-c094f9e61f034e029869900306b99827.ecs.us-east-1.on.aws")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
